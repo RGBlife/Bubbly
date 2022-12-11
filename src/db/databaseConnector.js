dece34d71e6977823ed12014be4ff76e5cf67179
Bret <bret_gomes811@hotmail.co.uk>
2022-12-11 11:29:47 +0000

    Change up connection

--- src/db/databaseConnector.js
+++ src/db/databaseConnector.js
@@ -6,10 +6,22 @@ export const connection = mysql.createConnection({
   database: process.env.DB_NAME
 })
 
-connection.connect()
+connection.connect(initialConnection);
+
+const initialConnection = (err) => {
+  if (err) throw err;
+  console.log("Connected to database");
+}
+
+const initialiseDatabase = () => {
+  connection.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+}
 
 // connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
 //   if (err) throw err
 
+    // })
 //   console.log('The solution is: ', rows[0].solution)
-// })
